4c01635a93b924499ec1881aefef49880eaf97bf
kichul kim <100783548+kichul7493@users.noreply.github.com>
2024-10-09 12:13:31 +0900

    Title: 알파벳에 따른 평가, Time: 352ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241009/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+char = input()
+
+if char is "S":
+    print("Superior")
+elif char is "A":
+    print("Excellent")
+elif char is "B":
+    print("Good")
+elif char is "C":
+    print("Usually")
+elif char is "D":
+    print("Effort")
+else:
+    print("Failure")
