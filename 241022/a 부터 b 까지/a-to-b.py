323ab3eb1f0b16d4de053f275d6b79ae6ef4bf85
kichul kim <100783548+kichul7493@users.noreply.github.com>
2024-10-22 20:52:22 +0900

    Title: a 부터 b 까지, Time: 236ms, Memory: 26MB, Status: Passed - CodeTree

--- 241022/a 부터 b 까지/a-to-b.py
+++ 241022/a 부터 b 까지/a-to-b.py
@@ -2,11 +2,9 @@ arr = input().split()
 
 a, b = int(arr[0]), int(arr[1])
 
-print(a, end=" ")
-
-while a < b:
+while a <= b:
+    print(a, end=" ")
     if a % 2 is 0:
         a += 3
     else:
         a *= 2
-    print(a, end=" ")
