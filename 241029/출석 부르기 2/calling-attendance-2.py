46427808e14375675ab142eda1a91a75521a8c5a
kichul kim <100783548+kichul7493@users.noreply.github.com>
2024-10-29 20:45:12 +0900

    Title: 출석 부르기 2, Time: 340ms, Memory: 79MB, Status: Passed - CodeTree

---
+++ 241029/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input())
+
+    if n is 1:
+        print("John")
+    elif n is 2:
+        print("Tom")
+    elif n is 3:
+        print("Paul")
+    elif n is 4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
